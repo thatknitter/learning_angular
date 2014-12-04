4099ed2ff6e729d39b4f74032376448397bba581
thatknitter <kblewis.flt@gmail.com>
2014-12-04 14:32:16 -0600

    now it does stuff

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-;(function(){
-  'use strict';
-  
-  var hello = 'world';
-}());
+;( function() {
+    'use strict';
+
+    angular.module('firstModule', []).controller('SimpleController', function($scope) {
+      var vm = this;
+      vm.name = 'Kimberly';
+      vm.friends = ['Cecil', 'Carlos', 'Dana', 'Marcus Vanston', 'Steve Carlsberg', 'Scout Master Harlan', 'Tamika Flynn', 'Them', 'Pamela Winchell', 'The Faceless Old Woman Who Secretly Lives in your House'];
+    });
+
+  }()); 
